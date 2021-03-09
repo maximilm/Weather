7ee7a1f96b8fd1a5e99cc34038f6692d927b8fd4
Just Man <N.Mikhail2002@gmail.com>
2021-03-10 01:05:19 +0300

    Try3

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -23,29 +23,11 @@ import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends Activity {
-    jghfughjghrughfugh
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-        docs = findViewById(R.id.docs);
-        adding = findViewById(R.id.adding);
-        elJur = findViewById(R.id.elJur);
-        ElJurLay = findViewById(R.id.ElJurLay);
-        startLay = findViewById(R.id.startLay);
-        docsLay = findViewById(R.id.docsLay);
-        addingLay = findViewById(R.id.addingLay);
-        choose_class_lay = findViewById(R.id.choose_class_lay);
-        tableDocsLay = findViewById(R.id.tableDocsLay);
-        teachers = findViewById(R.id.teachers);
-        startLay.setVisibility(View.VISIBLE);
-        /*docs.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                startLay.setVisibility(View.GONE);
-                docsLay.setVisibility(View.VISIBLE);
-            }
-        });*/
+
 
     }
 }
