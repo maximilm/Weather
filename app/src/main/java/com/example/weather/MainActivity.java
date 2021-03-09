09e9891ff2994ae141a3874d17c4962e14a05cb2
dmitrybot <mandal437@yandex.ru>
2021-03-10 01:04:46 +0300

    try2

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 public class MainActivity extends Activity {
     jghfughjghrughfugh
+    asdfghjkl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
