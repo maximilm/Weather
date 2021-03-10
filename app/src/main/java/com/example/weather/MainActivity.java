8c786ed3317a9c8eff65d1a590a7fcab4f359766
Just Man <N.Mikhail2002@gmail.com>
2021-03-11 01:12:56 +0300

    4

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.weather;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -11,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+
+    }
+
+    public void ChangeFragmentToList (View view) { //Меняет фрагмент с поиска на список городов
+        Fragment fragment;
+        //условие
+        //{
+
+        //}
+
+        //FragmentManager fm = getSupportFragmentManager();
+        //FragmentTransaction ft = fm.beginTransaction();
+        //ft.replace(R.id.search_fr, fragment);
+        //ft.commit();
     }
 
 
@@ -20,5 +38,8 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_main,menu);
         return true;
     }
+
+
+
 
 }
